a609cb134289414173b65b09fd0025ac191d6134
benharmonics <benjamin.jiron+git@gmail.com>
2022-07-11 15:55:55 -0600

    bugfixing & prettier formatting

--- decoder.py
+++ decoder.py
@@ -1,7 +1,8 @@
 #!/bin/python
 
+# Get input: either input the name of a file to be read or
+# the input text will be interpreted literally
 user_input = input('File or input text to decode: ')
-
 try:
     with open(user_input, 'r') as f:
         source_text = f.read()
@@ -11,42 +12,60 @@ except:
         print('No text entered.')
         quit()
 
-text = ''.join([c.lower() for c in source_text if c.isalpha()])
+text = ''.join([c.lower() for c in source_text])
 
+# get a list of ~100 of the most commonly used words in English
 with open('100commonwords.txt', 'r') as f:
     common_words = [word for word in f.read().split('\n') if not word.isspace()]
 
 def rotate_text(i, input_text):
+    """Caesar cipher: rotate text by i letters"""
     rotated_letters = []
     for c in input_text:
-        if ord(c) + i < ord('z'):
-            rotated_letters.append(chr(ord(c) + i))
+        if c.isalpha():
+            if ord(c) + i <= ord('z'):
+                letter = chr(ord(c) + i)
+            else:
+                overshoot = (ord(c) + i) - ord('z')
+                letter = chr(ord('a') + overshoot - 1)
         else:
-            overshoot = (ord(c) + i) - ord('z')
-            letter = chr(ord('a') + overshoot - 1)
-            rotated_letters.append(letter)
+            letter = c
+        rotated_letters.append(letter)
     return ''.join(rotated_letters)
 
-counts = []
+scores = []
 for i in range(26):
     test_text = rotate_text(i, text)
-
-    count = 0
+    score = 0
     for word in common_words:
-        count += test_text.count(word)
-    counts.append(count)
+        # weight scores toward longer words
+        score += test_text.count(word) * len(word)
+    scores.append(score)
+
+distribution = [s / sum(scores) for s in scores]
+
+# When we rotate 14 positions in our rotate_text function, if you think about
+# it, we're undoing 12 rotations in the encoding. So the encoding undone by
+# by our function is 26 minus the number of steps we took.
+percentages = [f'{round(100 * d, 1)}%' for d in reversed(distribution)]
+# The final percentage now corresponds to 0 rotations (the one before it corresponds
+# to 25 rotations). The first percentage corresponds to 1 rotation. Let's move the
+# last percentage to the front of the array for formatting
+percentages = [percentages[-1], *percentages[:-1]]
 
 steps_to_maxima = []
 best_guesses = []
-for (i, count) in enumerate(counts):
-    if count == max(counts):
+for (i, count) in enumerate(scores):
+    if count == max(scores):
         steps_to_maxima.append(i)
         best_guesses.append(rotate_text(i, text))
 
 print("(Each rotation increments the letter; e.g. a → b, d → e, z → a)")
 print(f'\nROTATION(S) WITH MOST HITS: {[26 - i for i in steps_to_maxima]}')
-print('Try decoding the text assuming a → b, b → c, et cetera these numbers of times.')
-print(f'HIT DISTRIBUTION: {counts}')
-print('BEST GUESS(ES):')
+print('Try decoding the text transformed (a → b, b → c, etc) assuming '
+      'the cipher was based on these numbers while being encoded.')
+print(f'\nSCORE DISTRIBUTION:\nROT 0-12: {percentages[:13]}\nROT 13-25: {percentages[13:]}')
+print('\nBEST GUESS(ES):')
 for guess in best_guesses:
     print(f'{guess}')
+
